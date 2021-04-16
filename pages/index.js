b6141dd420f16b7b93ac1cc78d6b10fe171b15ad
Alexandre Simas <alexandrecsimas@gmail.com>
2021-04-16 15:16:33 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,7 @@
-import { useState } from 'react';
-
 function Home(){
     return <div>
-                <h3>Home</h3>
-                <Contador />
-                <div>Teste</div>
+                <h3>Página Inicial</h3>
             </div>
-}
-
-function Contador(){
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador+1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div> 
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
 }
 
 export default Home
